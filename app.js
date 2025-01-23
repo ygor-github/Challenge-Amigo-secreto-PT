97df75f5c915c23de72f5865c6833feb2bc97ded
Ygor Hernández <ygorhernandezbr@gmail.com>
2025-01-23 15:35:52 -0300

    Função Adicionar Amigos
    Esta funçâo valida a entrada, atualiza o array de amigos e limpa o campo de entrada

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+let addAmigo = document.getElementById("amigo");
+
+function adicionarAmigo() {
+    let nome = addAmigo.value;
+    if ( nome != ""){
+        amigos.push(nome);
+        addAmigo.value = "";
+        console.log(amigos);
+        //listElements();
+        
+    } else{
+        alert("Por favor, insira um nome.")
+    }
+}
